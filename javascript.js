fba79a142bc098e15a8e87b5e91991694dd9c786
makiyudu <138406879+makiyudu@users.noreply.github.com>
2023-07-30 23:54:22 +0900

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,18 @@
+// ページ内リンク
+window.addEventListener('DOMContentLoaded', () => {
+    const anchorLinks = document.querySelectorAll('a[href^="#"]');
+    const anchorLinksArr = Array.prototype.slice.call(anchorLinks);
+  
+    anchorLinksArr.forEach(link => {
+      link.addEventListener('click', e => {
+        e.preventDefault();
+        const targetId = link.hash;
+        const targetElement = document.querySelector(targetId);
+        const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top;
+        window.scrollTo({
+          top: targetOffsetTop,
+          behavior: "smooth"
+        });
+      });
+    });
+  });
